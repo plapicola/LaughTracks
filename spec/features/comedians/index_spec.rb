5458ffa485587ebab33fcc09ce9df26f200a1652
plapicola <p.lapicola@gmail.com>
2019-01-26 16:07:36 -0700

    Adds tests for statistics area of page

--- spec/features/comedians/index_spec.rb
+++ spec/features/comedians/index_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe 'when I visit /comedians' do
       Comedian.create(name: "Two", birthplace: "Somewhere Else", age: 45)
       Comedian.create(name: "Three", birthplace: "Somewhere", age: 3)
     end
+
     it 'I see a list of all comedians' do
       visit '/comedians'
 
@@ -74,6 +75,36 @@ RSpec.describe 'when I visit /comedians' do
 
       within "#comedian-#{id}" do
         expect(page).to have_content("Runtime: #{runtime} minutes")
+      end
+    end
+
+    describe "I see a collection of statistics at the top of the page" do
+      it "including the averge age of comedians displayed" do
+        expected = "Average Age: 30"
+
+        within '#statistics' do
+          expect(page).to have_content(expected)
+        end
+      end
+
+      it "including the average run length of all specials on the page" do
+        Special.create(name: 'Uno', runtime: 35, comedian_id: Comedian.first.id)
+        Special.create(name: 'Dos', runtime: 45, comedian_id: Comedian.first.id)
+        Special.create(name: 'Dos', runtime: 40, comedian_id: Comedian.last.id)
+
+        expected = "Averge Runtime: 40 minutes"
+
+        within '#statistics' do
+          expect(page).to have_content(expected)
+        end
+      end
+
+      it "including a unique list of all cities displayed on the page" do
+        expected = "Somewhere, Somewhere Else"
+
+        within '#statistics' do
+          expect(page).to have_content(expected)
+        end
       end
     end
   end
