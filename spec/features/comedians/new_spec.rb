916b108c0526bd50bdb1c347a53f1018d321c0d6
plapicola <p.lapicola@gmail.com>
2019-01-27 11:31:29 -0700

    Adds tests for submitting form correctly redirecting, and redirected page displaying comedian

--- spec/features/comedians/new_spec.rb
+++ spec/features/comedians/new_spec.rb
@@ -8,5 +8,32 @@ RSpec.describe 'when I visit /comedians/new' do
       expect(page).to have_field("comedian[birthplace]")
       expect(page).to have_button("Submit")
     end
+
+    it "Submitting the form takes me back to the index" do
+      visit '/comedians/new'
+
+      page.fill_in("comedian[name]", with: "Tim")
+      page.fill_in("comedian[age]", with: "42")
+      page.fill_in("comedian[birthplace]", with: "Somewhere")
+      click_on("Submit")
+
+      expect(page.status_code).to eq(200)
+      expect(current_path).to eq('/comedians')
+    end
+
+    it "After submitting the form I see the comedian I created" do
+      visit '/comedians/new'
+
+      page.fill_in("comedian[name]", with: "Tim")
+      page.fill_in("comedian[age]", with: "42")
+      page.fill_in("comedian[birthplace]", with: "Somewhere")
+      click_on("Submit")
+
+      within('.comedian-info') do
+        expect(page).to have_content("Tim")
+        expect(page).to have_content("42")
+        expect(page).to have_content("Somewhere")
+      end
+    end
   end
 end
