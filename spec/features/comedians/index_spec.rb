3d9fd71bb86c1f587c5ddd3235abaf5b34b76451
plapicola <p.lapicola@gmail.com>
2019-01-26 19:43:01 -0700

    Adds test for query params on page

--- spec/features/comedians/index_spec.rb
+++ spec/features/comedians/index_spec.rb
@@ -57,6 +57,7 @@ RSpec.describe 'when I visit /comedians' do
 
     it "I see a list of each comedian's specials' names" do
       Special.create(name: 'Uno', runtime: 35, comedian_id: Comedian.first.id)
+
       visit '/comedians'
 
       id = Comedian.first.id
@@ -69,6 +70,7 @@ RSpec.describe 'when I visit /comedians' do
     it "I see a runtime in minutes for the comedian's specials" do
       runtime = 35
       Special.create(name: 'Uno', runtime: runtime, comedian_id: Comedian.first.id)
+
       visit '/comedians'
 
       id = Comedian.first.id
@@ -80,6 +82,9 @@ RSpec.describe 'when I visit /comedians' do
 
     describe "I see a collection of statistics at the top of the page" do
       it "including the averge age of comedians displayed" do
+
+        visit '/comedians'
+
         expected = "Average Age:"
 
         within '#average-age' do
@@ -92,6 +97,8 @@ RSpec.describe 'when I visit /comedians' do
         Special.create(name: 'Dos', runtime: 45, comedian_id: Comedian.first.id)
         Special.create(name: 'Dos', runtime: 40, comedian_id: Comedian.last.id)
 
+        visit '/comedians'
+
         expected = "Average Runtime:"
 
         within '#average-runtime' do
@@ -102,13 +109,24 @@ RSpec.describe 'when I visit /comedians' do
       it "including a unique list of all cities displayed on the page" do
         expected = "Somewhere Somewhere Else"
 
+        visit '/comedians'
+
         within '#city-list' do
           expect(page).to have_content(expected)
         end
       end
+    end
+    context "with query ?age=42" do
+      it "displays only comedians of age 42" do
 
-      context "with query age=42" do
+        visit '/comedians?age=42'
+        id = Comedian.first.id
 
+        within "#comedian-#{id}" do
+          expect(page).to have_content('One')
+        end
+        expect(page).to_not have_content("Two")
+        expect(page).to_not have_content("Three")
       end
     end
   end
